ea51a66235c168f17086d15049b1a0bed50fd711
Thibaud Robillard <thibaudrobillard@Thibauds-MacBook-Air.local>
2021-04-05 19:52:36 +0200

    homepage save1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
+const sneaker = document.querySelector(".pict img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
